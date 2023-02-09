a927f6db381b685374dca966cf1d750dbc93d448
kurozael <kurozael@gmail.com>
2023-02-09 02:01:56 +0000

    Simple chat

---
+++ code/ui/chatbox/Chatbox.cs
@@ -0,0 +1,43 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace Facepunch.BombRoyale.UI;
+
+public partial class Chatbox : Panel
+{
+	private static Chatbox Instance;
+
+	public Chatbox()
+	{
+		Instance = this;
+	}
+	
+	[ConCmd.Server]
+	public static void SendChat( string message )
+	{
+		if ( !ConsoleSystem.Caller.IsValid() ) return;
+
+		if ( ConsoleSystem.Caller.Pawn is BombRoyalePlayer player )
+		{
+			AddChat( To.Everyone, ConsoleSystem.Caller.Name, player.GetTeamColor(), message );
+		}
+	}
+
+	[ConCmd.Server( "br.chat.system" )]
+	public static void AddSystemMsgCmd( string msg )
+	{
+		AddSystem( msg );
+	}
+
+	[ClientRpc]
+	public static void AddSystem( string message )
+	{
+		Instance.AddMessage( message, "system" );
+	}
+
+	[ConCmd.Client( "br.say", CanBeCalledFromServer = true )]
+	public static void AddChat( string name, Color color, string message )
+	{
+		Instance.AddNamedMessage( name, color, message );
+	}
+}

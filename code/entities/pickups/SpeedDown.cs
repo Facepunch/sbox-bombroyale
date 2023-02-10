eb4e463da85e101f6c43796b04e7cbfaab9c0cf9
kurozael <kurozael@gmail.com>
2023-02-10 10:11:57 +0000

    Add speed down

---
+++ code/entities/pickups/SpeedDown.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox;
+using System;
+
+namespace Facepunch.BombRoyale;
+
+[PickupChance( 0.3f )]
+public partial class SpeedDown : Pickup
+{
+	public override string PickupSound => "pickup.bad";
+	public override string Icon => "textures/speeddown.png";
+	public override Color Color => Color.Red;
+
+	protected override void OnPickup( BombRoyalePlayer player )
+	{
+		player.SpeedBoosts = Math.Max( player.SpeedBoosts - 1, 0 );
+	}
+}

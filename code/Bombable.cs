32da0215704093e496997361a1ab35c0268b69e0
kurozael <kurozael@gmail.com>
2024-02-09 12:54:01 +0000

    Bombable

---
+++ code/Bombable.cs
@@ -0,0 +1,50 @@
+﻿using Sandbox;
+using Editor;
+
+namespace Facepunch.BombRoyale;
+
+public class Bombable : Component
+{
+	[Sync] public bool IsHidden { get; set; }
+	
+	public bool IsSpaceOccupied()
+	{
+		if ( !IsHidden ) return true;
+
+		var renderer = Components.Get<ModelRenderer>();
+		var bounds = renderer.Bounds + Transform.Position;
+		
+		var trace = Scene.Trace.Ray( bounds.Center, bounds.Center )
+			.Radius( 8f )
+			.WithAnyTags( "solid", "pickup", "player", "bomb" )
+			.Run();
+
+		return trace.Hit;
+	}
+
+	public void TrySpawnPickup()
+	{
+		/*
+		if ( Game.Random.Float() < 0.35f )
+		{
+			var p = Pickup.CreateRandom();
+			p.Position = WorldSpaceBounds.Center;
+			return;
+		}
+		*/
+	}
+
+	public void Hide()
+	{
+		var renderer = Components.Get<ModelRenderer>();
+		renderer.Enabled = false;
+		IsHidden = true;
+	}
+
+	public void Show()
+	{
+		var renderer = Components.Get<ModelRenderer>();
+		renderer.Enabled = true;
+		IsHidden = false;
+	}
+}
---
+++ code/DiseaseType.cs
@@ -0,0 +1,30 @@
+﻿using Sandbox;
+using Editor;
+
+namespace Facepunch.BombRoyale;
+
+public enum DiseaseType
+{
+	None,
+	MoveSlow,
+	MoveFast,
+	RandomBomb,
+	Teleport,
+	LowRange
+}
+
+public static class DiseaseTypeExtensions
+{
+	public static string GetName( this DiseaseType type )
+	{
+		return type switch
+		{
+			DiseaseType.MoveSlow => "⏪ Move Slow",
+			DiseaseType.MoveFast => "⏩ Move Fast",
+			DiseaseType.RandomBomb => "💩 Drop Random Bombs",
+			DiseaseType.Teleport => "🔀 Swap Places",
+			DiseaseType.LowRange => "💣 Minimum Bomb Range",
+			_ => "Nothing",
+		};
+	}
+}

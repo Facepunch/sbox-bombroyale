9bc807ab037c96f2754ea0c48689faeadd9d22d9
kurozael <kurozael@gmail.com>
2024-02-12 12:12:05 +0000

    Add Round

---
+++ code/Round.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox;
+using Editor;
+
+namespace Facepunch.BombRoyale;
+
+public enum Round
+{
+	Lobby,
+	Playing,
+	GameOver
+}
+
+public static class RoundExtension
+{
+	public static string GetName( this Round round )
+	{
+		return round switch
+		{
+			Round.Lobby => "Lobby",
+			Round.Playing => "Playing",
+			Round.GameOver => "Game Over",
+			_ => string.Empty
+		};
+	}
+}
